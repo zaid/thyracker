088948e4782e049fe11acc335513910ffee45d03
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2012-03-01 22:03:08 -0500

    use mongodb with mongoid for all environments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,17 +31,28 @@ Spork.prefork do
     # config.mock_with :rr
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+    # config.use_transactional_fixtures = false
 
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+
+    require 'database_cleaner'
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.orm = "mongoid"
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.clean
+    end
   end
 end
 
