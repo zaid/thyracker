23b12b14386eb44a306f92fa937770d754e93844
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2012-03-01 20:27:31 -0500

    initialize a new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thyracker::Application.config.secret_token = '827549d345b99f8b16fc570f362e13be44f35b94a22df0ab74054e1ccbb2372532d11041a37a7cdbbd9b83cde77bbdd3ebd3e731e7cf4bb1760470bf2f4271d6'
