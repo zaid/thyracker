13caa89856f9e71644a6f8dedb56f65b1471f3bc
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2012-08-15 19:34:50 -0400

    Run unicorn as zaid for now

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/zaid/apps/thyracker/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=zaid
 set -u
 
 OLD_PIN="$PID.oldbin"
