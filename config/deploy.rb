89277b6f9b3c56ae644994763557a42af466a661
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2012-08-15 19:49:17 -0400

    Symlink mongoid.yml from the shared directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
   end
+  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
